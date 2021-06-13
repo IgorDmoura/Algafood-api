6ab7888979e10a85d43b85470ba96be76f5873a2
IgorDmoura <79521665+IgorDmoura@users.noreply.github.com>
2021-06-13 16:52:25 -0300

    Profiles e DI

---
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.algaworks.algafood.modelo.Cliente;
+import com.algaworks.algafood.service.AtivacaoClienteService;
+
+@Controller
+public class MeuPrimeiroController {
+	
+	private AtivacaoClienteService ativacaoClienteService;
+	
+	public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+		this.ativacaoClienteService = ativacaoClienteService;
+	}
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		Cliente joao = new Cliente("João","joão@xyz.com","34653565");
+		ativacaoClienteService.ativar(joao);
+		return "Hello";
+	}
+
+}
